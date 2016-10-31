ece5ca327173028a24d564385695dad052a478e3
Yaniv Izaki <Yaniv Izaki>
2016-10-31 11:18:28 +0200

    add main project files

---
+++ script.js
@@ -0,0 +1,14 @@
+var sex = prompt("Are you a boy or a girl?..")
+var age = prompt("What's your age?")
+if (sex === "girl" && age>=18) 
+	{
+		alert("Welcome to free ladies nigt!")
+	} 
+	else if (age>=18) 
+	{
+		alert("Guys pay..")
+	}
+	else
+	{
+		alert("come back at 18..")
+	}
